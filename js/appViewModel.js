3fa2735c7e8b4d659a55940a71278aa6e19020e3
maxim-popkov <maxim.popkov@outlook.com>
2013-10-08 01:50:44 +0400

    Сделана выдача списка объектов на страницу, поиск по ним, переход на объект и возвращение обратно к списку appViewModel.js ---- Добавлены Объекты: 1) pages - страницы навигации 2) currentPage - активная страница 3) students - список всех студентов, массив с привязками, поле visible=true - если по критерию поиска студент должен быть в выдаче 4) studentsList - вычисляемый массив попадающий в результат выдачи 5) saveStudents - сохраненный массив студентов, сохраняется при переключении страниц 6) chosenStudent - выбранный студент 7) search - значение поля поиска Методы: 1) previewPage - показать страницу 2) previewStudent - показать студента Подписчик: 1) search - следит за обновлением строки поиска
    index.html
    ----
    Добавлены:
    1) class Search - строка поиска
    2) class viewStudent - шаблон для просмотра студента

--- js/appViewModel.js
+++ js/appViewModel.js
@@ -1,9 +1,66 @@
 // Основная логика Приложения
 define(['knockout-2.3.0'],function (ko) {
 	return function appViewModel () {
-		this.firstName = ko.observable('Ivan');
-		this.firstNameCaps = ko.computed(function () {
-			return this.firstName().toUpperCase();
+		this.pages = ['main', 'student'];
+		this.currentPage = ko.observable('main');
+
+		// сохранить студентов перед сменой страницы
+		this.saveStudents = [];
+
+		// список студентов
+		this.students = ko.observableArray([
+				{name: 'nm1', lastName: 'lnm1', visible: ko.observable(true)},
+				{name: 'nm2', lastName: 'lnm2', visible: ko.observable(true)},
+				{name: 'nm3', lastName: 'lnm3', visible: ko.observable(true)},
+				{name: 'nm4', lastName: 'lnm4', visible: ko.observable(true)},
+			]);
+		// строка поиска
+		this.search = ko.observable();
+
+		// список студентов в ответе страницы
+		this.studentsList = ko.computed(function(){
+			console.log('studentsList recalc');
+			return ko.utils.arrayFilter(this.students(), function(student){
+				return student.visible();
+			});
+		},this);
+		
+		// подписка и действие при обновлении строки поиска
+		this.search.subscribe(function(){
+			console.log(1);
+			console.log(this.search());
+			ko.utils.arrayForEach(this.students(), function (student) {
+				var regex = new RegExp("^" + this.search().toUpperCase());
+				var name = student.name.toUpperCase();
+				student.visible(regex.test(name));
+			}.bind(this));
 		}, this);
+
+		// выбранный студент
+		this.chosenStudent = ko.observable();
+
+		// показать страницу
+		this.previewPage = function (page) {
+			console.log('page: ', page);
+			switch(page){
+				case 'main':
+					this.chosenStudent(null);
+					console.log(this);
+					this.students(this.saveStudents);
+			}
+		}.bind(this);
+
+		// показать студента
+		this.previewStudent = function(student){
+			console.log('preview student');
+			console.log(student);
+			this.saveStudents = this.students();
+			this.students(null);
+			console.log(this.saveStudents);
+			this.chosenStudent(student);
+		}.bind(this);
+
+		// при первом запуске ни один студент не выбран
+		this.chosenStudent(null);
 	};
 });
