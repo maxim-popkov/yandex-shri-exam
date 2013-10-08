be7165a6b1d7e4d363020d50b168c63eac857146
maxim-popkov <maxim.popkov@outlook.com>
2013-10-08 20:02:22 +0400

    Добавлена работа с несколькими вкладками, вьюшки меняются в зависимотсти от выбранной вкладки studentList теперь проверяет поле поиска и пересчитывается, только когда активна вкладка main
    appViewModel
    ----
    Добавлены:
    saveLections - список лекций
    lections - список лекций для отображения
    chosenLection - выбранная лекция
    
    saveShri - информация о Шри /заглушка
    shri - информация о Шри для отображения
    
    Изменены:
    previewPage - добавлена работа с несколькими вкладками
    
    теперь по умолчанию основной список данных хранится в объектах с префиксом save, обозреваемые массивы используются для имитации перехода между вкладками
    
    index.html
    добавлены div для отображения:
    1) Лекций
    2) Информации о Шри

--- js/appViewModel.js
+++ js/appViewModel.js
@@ -1,19 +1,22 @@
 // Основная логика Приложения
 define(['knockout-2.3.0'],function (ko) {
 	return function appViewModel () {
-		this.pages = ['main', 'student'];
+		/************************
+		 * Объявления переменных
+		 ************************/
+		this.pages = ['main', 'lections', 'shri'];
 		this.currentPage = ko.observable('main');
 
 		// сохранить студентов перед сменой страницы
-		this.saveStudents = [];
-
-		// список студентов
-		this.students = ko.observableArray([
+		this.saveStudents = [
 				{name: 'nm1', lastName: 'lnm1', visible: ko.observable(true)},
 				{name: 'nm2', lastName: 'lnm2', visible: ko.observable(true)},
 				{name: 'nm3', lastName: 'lnm3', visible: ko.observable(true)},
 				{name: 'nm4', lastName: 'lnm4', visible: ko.observable(true)},
-			]);
+			];
+
+		// список студентов
+		this.students = ko.observableArray();
 		// выбранный студент
 		this.chosenStudent = ko.observable();
 
@@ -22,12 +25,36 @@ define(['knockout-2.3.0'],function (ko) {
 
 		// список студентов в ответе страницы
 		this.studentsList = ko.computed(function(){
-			console.log('studentsList recalc');
-			return ko.utils.arrayFilter(this.students(), function(student){
-				return student.visible();
-			});
+			if (this.currentPage() == 'main') {
+					console.log('studentsList recalc');
+						return ko.utils.arrayFilter(this.students(), function(student){
+					return student.visible();
+				});
+			}
 		},this);
 		
+		// сохранить лекции перед сменой страницы
+		this.saveLections = [
+				{header: 'lec1', lector: 'lector1'},
+				{header: 'lec2', lector: 'lector2'},
+				{header: 'lec3', lector: 'lector3'},
+			];
+
+		// список лекций
+		this.lections = ko.observableArray();
+		
+		// выбранная лекция
+		this.chosenLection = ko.observable();
+
+		// cохраненная информация о Шри
+		this.saveShri = {info: 'shri info', details: 'shri details'};
+
+		// Про Шри
+		this.shri = ko.observable();
+
+		/********************
+		 * Логика приложения
+		 ********************/
 		// подписка и действие при обновлении строки поиска
 		this.search.subscribe(function(){
 			console.log(1);
@@ -44,9 +71,28 @@ define(['knockout-2.3.0'],function (ko) {
 			console.log('page: ', page);
 			switch(page){
 				case 'main':
+					this.chosenLection(null);
+					this.lections(null);
 					this.chosenStudent(null);
-					console.log(this);
+					this.shri(null);
 					this.students(this.saveStudents);
+					break;
+				case 'lections':
+					this.chosenLection(null);
+					this.chosenStudent(null);
+					this.students(null);
+					this.shri(null);
+					this.lections(this.saveLections);
+					console.log(this.lections());
+					break;
+				case 'shri':
+					this.chosenLection(null);
+					this.chosenStudent(null);
+					this.students(null);
+					this.lections(null);
+					this.shri(this.saveShri);
+					console.log(this.shri());
+					break;
 			}
 		}.bind(this);
 
@@ -62,5 +108,6 @@ define(['knockout-2.3.0'],function (ko) {
 
 		// при первом запуске ни один студент не выбран
 		this.chosenStudent(null);
+		this.students(this.saveStudents);
 	};
 });
